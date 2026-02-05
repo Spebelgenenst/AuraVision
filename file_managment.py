152bc4bc3d188388a63129f586e2f351a50ebf65
spebelgenenst <sebastian@die-sperlinge.de>
2026-02-05 08:36:38 +0100

    modified:   file_managment.py

--- file_managment.py
+++ file_managment.py
@@ -1,9 +1,16 @@
-import zarr
-import numpy as np
+from pymilvus import MilvusClient
 
 class database():
     def __init__(self):
-        pass
+        client = MilvusClient("embeds.db")
+
+        if client.has_collection(collection_name="embeds"):
+            client.drop_collection(collection_name="embeds")
+        client.create_collection(
+            collection_name="embeds",
+            dimension=2,  # The vectors we will use in this demo has 768 dimensions
+)
+
 
     def get_embed(self, num):
         return
