62a334e884f2533077063388ea39f0c4311ee179
Spebelgenenst <sebastian@die-sperlinge.de>
2026-02-05 23:27:00 +0100

    fixed a bug

--- database.py
+++ database.py
@@ -22,7 +22,6 @@ class database():
         return result
 
     def add_entry(self, embed, url, link, credit, source): # add entry with url and embed
-        self.last_id += 1
 
         data = [{"auto_id": True, "vector": embed, "url": url, "link": link, "credit": credit, "source": source}]
 
