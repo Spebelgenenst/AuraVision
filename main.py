f2003e0347c9e4e8970c7f9782146bcb5676dea4
Spebelgenenst <sebastian@die-sperlinge.de>
2026-02-05 21:00:55 +0100

    added function to colect data from unsplash

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from ai import gen_embeds
-from file_managment import database
+from database import database
 from pictures import unsplash
 
 db = database()
 embeds = gen_embeds()
 pic = unsplash()
+
+class main():
+    pass
+
+class search():
+    pass
+
+class new_data():
+    def unsplash(self):
+        photo = pic.get_random()
+        image = pic.download_image(photo=photo)
+
+        embed = embeds.image_embed(image=image)
+
+        database.add_entry(embed=embed, url=photo["links"]["html"], link=photo["urls"]["raw"], credit=[ photo["user"]["name"] , photo["user"]["username"] ], source="unsplash")
+
