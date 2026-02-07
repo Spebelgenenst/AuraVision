986f422464e357c97ff4de83d7c26f6335f4cac9
Spebelgenenst <sebastian@die-sperlinge.de>
2026-02-07 15:50:28 +0100

    finished collect_data unsplash

--- main.py
+++ main.py
@@ -2,31 +2,28 @@ from ai import gen_embeds
 from database import database
 #from pictures import unsplash
 from pictures_fake import unsplash
+import hashlib
 
 db = database()
 embeds = gen_embeds()
 pic = unsplash()
 
-class main():
-    pass
-
 class search():
     pass
 
-class new_data():
+class collect_data():
     def __init__(self):
-        for x in range(0, 100):
-            for y in range(0, 100):
-                self.unsplash()
-            print(x)
+        return
 
     def unsplash(self):
         photo = pic.get_random()
-
-        image = pic.download_image(photo=photo)
+        if photo:
+            image = pic.download_image(photo=photo)
+        else:
+            return True #returns if rate limit is reached
 
         embed = embeds.image_embed(image=image)
 
         db.add_entry(embed=embed, url=photo["urls"]["raw"], link=photo["links"]["html"], credit=[ photo["user"]["name"] , photo["user"]["username"] ], source="unsplash")
 
-new_data()
+data = collect_data()
